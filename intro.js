3a626879601b9f7aa55b2b083983b7275ebce052
Wasiu Olalekan <103901696+QA-WASIU@users.noreply.github.com>
2026-08-14 07:55:35 +0100

    Update intro.js
    Make changes in the console.log.

--- intro.js
+++ intro.js
@@ -4,6 +4,6 @@ const age = 44;
 const favoriteProgrammingLanguage = "JavaScript";
 
 // Log the values to the console
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
+console.log(name);
+console.log(age);
+console.log(favoriteProgrammingLanguage);
