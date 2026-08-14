de75c1d67e0ba9fb7031a73d9476339758979dcb
Wasiu <wasiuo72@gmail.com>
2026-08-14 07:32:55 +0100

    First Commit:Betechified Week1 Assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+// My short Bio
+const name = "Wasiu";
+const age = 44;
+const favoriteProgrammingLanguage = "JavaScript";
+
+// Log the values to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
